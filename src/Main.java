89a3f0faa9799f04cb3b8eeda4647658427e3c1a
isaccanedo <isaccanedo@gmail.com>
2021-01-12 09:24:51 -0300

    :cat: Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ *  What is the output of this code? (Qual é a saída deste código?)
+ * 
+ *  1 - truefalse
+ *  2 - The code does not compile
+ *  3 - truetrue ==> correct
+ *  4 - falsetrue
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		List list = new ArrayList();
+		list.add("hello");
+		list.add(2);
+		System.out.println(list.get(0) instanceof Object);
+		System.out.println(list.get(1) instanceof Integer);
+
+	}
+
+}
